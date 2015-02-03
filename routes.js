a41b9cc981abae7cc65c7caf278e4e100c6ab9b1
madhav <madhav89@gmail.com>
2015-02-02 21:36:58 -0500

    fix ws

--- routes.js
+++ routes.js
@@ -7,19 +7,19 @@ module.exports = function (app) {
   });
 
   app.post('/scrape', function (req, res) {
-  	var reqUrl = req.body.url;
+    var reqUrl = req.body.url;
     if (reqUrl && reqUrl.indexOf('http') != 0) {
       reqUrl = 'http://' + reqUrl;
     }
-  	if (!reqUrl) {
-  		res.status(400).send('URL parameter is required, got ' + reqUrl);
-  	} else {
+    if (!reqUrl) {
+      res.status(400).send('URL parameter is required, got ' + reqUrl);
+    } else {
       var parsedUrl = url.parse(reqUrl);
       if (parsedUrl.host && parsedUrl.host.indexOf('lonelyplanet.com') < 0) {
         console.error('Cannot scrape ' + reqUrl);
         res.status(400).send('URL is not supported ' + reqUrl)
       }
-  		lpScraper(reqUrl, res);
-  	}
+      lpScraper(reqUrl, res);
+    }
   });
 };
